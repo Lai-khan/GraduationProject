2e32e29b453e9505fffe5630a54081a1509f8547
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2019-10-24 18:09:03 +0900

    BE 게시판 & 기타반주페이지
    1. 게시판 DB에서 데이터 불러오기
    2. 각각 기타반주 페이지와 연결
    + 기타프로파일 추가(gp3~5, gpx파일 다 됨)

--- GuitarMusicDevice/routes/guitar.js
+++ GuitarMusicDevice/routes/guitar.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/mysql');
 
-router.get('/', function(req, res, next) {
-    res.render('guitar');
+router.get('/:file', function(req, res, next) {
+    var file = req.params.file;
+    console.log(file);
+    var sql = `SELECT filename FROM music WHERE idx = ${file}`;
+    db.query(sql, function(err, filename) {
+        if(err) next(err);
+        else {
+            console.log(filename);
+            res.render('guitar', {guitarPro: filename});
+        }
+    })
 });
 
 module.exports = router;
--- GuitarMusicDevice/routes/music.js
+++ GuitarMusicDevice/routes/music.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 var db = require('../lib/mysql');
 
-router.get('/', function(req, res, next) {
-    res.render('board');
+router.get('/page/:page', function(req, res, next) {
+    var page = req.params.page;
+    var sql = `SELECT idx, title, name, date_format(updateDate,'%Y-%m-%d') updateDate FROM board`;
+    db.query(sql, function(err, list) {
+        if(err) next(err);
+        else {
+            console.log(list);
+            res.render('board', {list: list, page: page, page_num:10});
+        }
+    })
 });
 
 module.exports = router;
