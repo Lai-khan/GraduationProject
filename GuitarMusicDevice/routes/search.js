063bfe588a5fc7bf7a7271faa94197585770187f
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2019-10-25 20:35:25 +0900

    BE 검색 기능

--- GuitarMusicDevice/routes/search.js
+++ GuitarMusicDevice/routes/search.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 var db = require('../lib/mysql');
 
-router.get('/?:musicName', function(req, res, next) {
-    //  DB Query
+router.get('/:page', function(req, res, next) {
+    var search = req.param("musicName");
+    var page = req.params.page;
+    var sql = `
+    SELECT idx, title, name, date_format(updateDate,'%Y-%m-%d') updateDate 
+    FROM board 
+    WHERE title LIKE "%${search}%" OR name LIKE "%${search}%"`;
+    console.log(search);
+    console.log(page);
+    console.log(sql);
+    db.query(sql, function(err, result) {
+        if(err) next(err)
+        else {
+            // console.log(result);
+            res.render('board', {list: result, page: page, page_num: 10});
+        }
+    })
 });
 
 module.exports = router;
