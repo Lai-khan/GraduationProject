d103aedbafd249d5e4423553a5e2d2f2f753fe72
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2019-10-27 18:09:44 +0900

    BE 페이징

--- GuitarMusicDevice/routes/music.js
+++ GuitarMusicDevice/routes/music.js
@@ -4,12 +4,13 @@ var db = require('../lib/mysql');
 
 router.get('/page/:page', function(req, res, next) {
     var page = req.params.page;
+    var url = '/music/page/';
     var sql = `SELECT idx, title, name, date_format(updateDate,'%Y-%m-%d') updateDate FROM board`;
     db.query(sql, function(err, list) {
         if(err) next(err);
         else {
             console.log(list);
-            res.render('board', {list: list, page: page, page_num:10});
+            res.render('board', {list: list, page: page, page_num: 10, url: url, state: 1});
         }
     })
 });
--- GuitarMusicDevice/routes/search.js
+++ GuitarMusicDevice/routes/search.js
@@ -3,20 +3,18 @@ var router = express.Router();
 var db = require('../lib/mysql');
 
 router.get('/:page', function(req, res, next) {
-    var search = req.param("musicName");
+    var search = req.param("search");
     var page = req.params.page;
+    var url = '/search/';
     var sql = `
     SELECT idx, title, name, date_format(updateDate,'%Y-%m-%d') updateDate 
     FROM board 
     WHERE title LIKE "%${search}%" OR name LIKE "%${search}%"`;
-    console.log(search);
-    console.log(page);
-    console.log(sql);
     db.query(sql, function(err, result) {
         if(err) next(err)
         else {
-            // console.log(result);
-            res.render('board', {list: result, page: page, page_num: 10});
+            console.log(result);
+            res.render('board', {list: result, page: page, page_num: 10, url: url, searchGet: search, state: 2});
         }
     })
 });
