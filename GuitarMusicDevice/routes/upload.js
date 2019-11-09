2b3fe7cd78c9db01d7fd8d7328e7e45a7452ea7b
Guardian_demoN <xman0425@naver.com>
2019-11-09 19:40:51 +0900

    uploads
    god dang it directory thingy thingy
    (upload implementing, not fully one, but functionally one)

--- GuitarMusicDevice/routes/upload.js
+++ GuitarMusicDevice/routes/upload.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 var db = require('../lib/mysql');
 
+var multer = require('multer');
+
+let storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './uploads');
+    },
+    filename: function(req, file, callback){
+        callback(null, file.originalname);
+    }
+})
+
+let upload = multer({
+    storage: storage
+})
+
 router.get('/', function(req, res, next) {
-    res.render('upload');
+    res.render('upload2');
+});
+
+router.post('/process', upload.single('gpfile'), function(req, res, next){
+    res.send('uploaded file <br />' + req.file);
+    console.log(req.file);
 });
 
 module.exports = router;
