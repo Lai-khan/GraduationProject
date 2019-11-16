24103a670b910c6b243064c28a3e0b525b61500d
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2019-11-16 18:32:39 +0900

    마이페이지 폼 : 회원정보, 회원탈퇴

--- GuitarMusicDevice/routes/mypage.js
+++ GuitarMusicDevice/routes/mypage.js
@@ -12,7 +12,7 @@ router.get('/sheet/:page', function(req, res, next) {
         if(err) next(err);
         else {
             console.log(result);
-            res.render('mypage', {list : result, page: page, page_num: 10, url: url});
+            res.render('mypage', {isLogined : true, list : result, page: page, page_num: 10, url: url});
         }
     })
 });
@@ -25,21 +25,25 @@ router.post('/delete_process', function(req, res, next) {
 
 router.get('/info', function(req, res, next) {
     var state = "info";
-    res.render('inputPW', {state, state});
+    res.render('inputPW', {isLogined : true, state, state});
 });
 
 router.post('/info/process', function(req, res, next) {
-    res.send("your info");
+    res.redirect('myinfo')
+});
+
+router.get('/info/myinfo', function(req, res, next) {
+    res.render('info', {isLogined : true});
 });
 
 router.get('/leave', function(req, res, next) {
     var state = "leave";
-    res.render('inputPW', {state, state});
+    res.render('inputPW', {isLogined : true, state, state});
 });
 
 router.post('/leave/really', function(req, res, next) {
     console.log(req.body.password);
-    res.send("leave Really?");
+    res.render('leave', {isLogined : true});
 });
 
 module.exports = router;
